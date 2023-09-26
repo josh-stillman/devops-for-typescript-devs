024bd572de4c3c92702a8c89a7280ee88aacaa14
Josh Stillman <josh.stillman@giantmachines.com>
2023-09-26 12:56:54 -0400

    add load balancer

--- src/backend/backend.ts
+++ src/backend/backend.ts
@@ -4,17 +4,53 @@ import * as awsx from '@pulumi/awsx';
 import { BACKEND_SECRETS } from './backendSecrets';
 import { Role } from '@pulumi/aws/iam';
 import { createBackendPipelineUser } from '../iam/pipelineUser';
-
-const config = new pulumi.Config('api');
-const containerPort = config.getNumber('containerPort') || 80;
-const cpu = config.getNumber('cpu') || 512;
-const memory = config.getNumber('memory') || 128;
+const containerPort = apiConfig.getNumber('containerPort') || 80;
+const containerName = apiConfig.get('containerName') || 'dev-backend-container';
+const cpu = apiConfig.getNumber('cpu') || 512;
+const memory = apiConfig.getNumber('memory') || 128;
 
 // An ECS cluster to deploy into
 const cluster = new aws.ecs.Cluster('cluster', {});
 
 // An ALB to serve the container endpoint to the internet
-const loadBalancer = new awsx.lb.ApplicationLoadBalancer('loadbalancer', {});
+const loadBalancer = new awsx.lb.ApplicationLoadBalancer('loadbalancer', {
+  listener: {
+    certificateArn: getARN(getExistingCertificate(domain)),
+    port: 443,
+    protocol: 'HTTPS',
+    sslPolicy: 'ELBSecurityPolicy-2016-08',
+  },
+  defaultSecurityGroup: {
+    args: {
+      ingress: [
+        {
+          fromPort: 443,
+          protocol: 'tcp',
+          toPort: 443,
+          cidrBlocks: ['0.0.0.0/0'],
+        },
+        {
+          fromPort: 80,
+          protocol: 'tcp',
+          toPort: 80,
+          cidrBlocks: ['0.0.0.0/0'],
+        },
+      ],
+    },
+  },
+  defaultTargetGroup: {
+    port: 80,
+    protocol: 'HTTP',
+    targetType: 'ip',
+    healthCheck: {
+      enabled: true,
+      matcher: '200-299',
+      path: '/_health',
+      interval: 60 * 3,
+      protocol: 'HTTP',
+    },
+  },
+});
 
 // An ECR repository to store our application's container image
 const repo = new awsx.ecr.Repository('repo', {
@@ -51,6 +87,7 @@ const taskDefinition = new awsx.ecs.FargateTaskDefinition('api-task-def', {
     essential: true,
     portMappings: [
       {
+        hostPort: containerPort,
         containerPort: containerPort,
         targetGroup: loadBalancer.defaultTargetGroup,
       },
@@ -86,6 +123,13 @@ const service = new awsx.ecs.FargateService('service', {
   cluster: cluster.arn,
   assignPublicIp: true,
   taskDefinition: taskDefinition.taskDefinition.arn,
+  loadBalancers: [
+    {
+      containerName: containerName,
+      containerPort: containerPort,
+      targetGroupArn: loadBalancer.defaultTargetGroup.arn,
+    },
+  ],
 });
 
 const user = createBackendPipelineUser(
